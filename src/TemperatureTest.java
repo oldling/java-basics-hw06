15e66ec9a5bb7607f9c4f0010eacf2769ff91b56
oldling <oldling@gmail.com>
2022-06-12 15:24:29 +0300

    Temp calc works

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,10 @@ public class TemperatureTest {
     return new Object[][]{
         {212.0, 100.0},
         {32.0, 0.0},
-        {-15, -26.11111111111111}
-        // TODO add 2 more test data here
+        {-15, -26.11111111111111},
+        {-459.67, -273.15},
+        {225.5, 107.5}
+
     };
   }
 
@@ -28,8 +30,10 @@ public class TemperatureTest {
     return new Object[][]{
         {100.0, 212.0},
         {0.0, 32.0},
-        {-26.11111111111111, -15}
-        // TODO add 2 more test data here
+        {-26.11111111111111, -15},
+        {-273, -459.4},
+        {9999.0, 18030.20}
+
     };
   }
 
